b86c5cea9f0fed1cfd48d122298164a03a2e8136
Simonebaca <simobaco99@gmail.com>
2023-05-16 17:37:56 +0200

    add class Genre

--- index.php
+++ index.php
@@ -43,6 +43,14 @@ class Movie {
     }
 }
 class Genre {
-    
-   
+    public $name;
+    public function __construct($name) {
+        $this->name = $name;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
 }
+
+   
